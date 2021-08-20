7cec72b677bb68b2fa055aa4a7e184ac6bd99c16
sean <gmlssns5859@gmail.com>
2021-08-20 11:15:38 +0900

    solve

--- baekjoon1037.py
+++
@@ -1,5 +0,0 @@
-N = int(input())
-
-leaknum = list(map(int, input().split()))
-
-print(max(leaknum)*min(leaknum))
--- baekjoon1654.py
+++
@@ -1,7 +0,0 @@
-N, K = map(int,input().split())
-B = 0
-
-for i in range(N):
-    A = int(input())
-    B += A
-print(B)
